a1a258e31509516ee2bcf40be97a1544be8f7dcd
Akhila Madathil <madathilakhila@gmail.com>
2018-12-11 18:22:29 +0530

    view position check

--- FlappyBirdsDemo/GameScene.swift
+++ FlappyBirdsDemo/GameScene.swift
@@ -9,6 +9,10 @@
 import SpriteKit
 import GameplayKit
 
+enum Layer: CGFloat {
+    case background
+    case foreground
+}
 class GameScene: SKScene {
     let worldNode = SKNode()
     var playableStart: CGFloat = 0
@@ -16,12 +20,15 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         addChild(worldNode)
+        setupBackground()
+        setupForeground()
     }
     
-    func setupBackgroun() {
-        let background = SKSpriteNode(imageNamed: "Background")
+    func setupBackground() {
+        let background = SKSpriteNode(imageNamed: "background")
         background.anchorPoint = CGPoint(x: 0.5, y: 1.0)
         background.position = CGPoint(x: size.width/2, y: size.height)
+        background.zPosition = Layer.background.rawValue
         playableStart = size.height - background.size.height
         playableheight = background.size.height
         worldNode.addChild(background)
@@ -31,5 +38,6 @@ class GameScene: SKScene {
         let foreground = SKSpriteNode(imageNamed: "Ground")
         foreground.anchorPoint = CGPoint(x: 0.0, y: 1.0)
         foreground.position = CGPoint(x: size.width/2, y: playableStart)
+        foreground.zPosition = Layer.foreground.rawValue
     }
 }
