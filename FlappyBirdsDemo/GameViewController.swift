eb59599a248cf886f9eaa20d7c16a5f730262985
Akhila Madathil <madathilakhila@gmail.com>
2018-12-11 17:52:14 +0530

    partial commit

--- FlappyBirdsDemo/GameViewController.swift
+++ FlappyBirdsDemo/GameViewController.swift
@@ -12,36 +12,51 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+//        // including entities and graphs.
+//        if let scene = GKScene(fileNamed: "GameScene") {
+//            
+//            // Get the SKScene from the loaded GKScene
+//            if let sceneNode = scene.rootNode as! GameScene? {
+//                
+//                // Copy gameplay related content over to the scene
+//                sceneNode.entities = scene.entities
+//                sceneNode.graphs = scene.graphs
+//                
+//                // Set the scale mode to scale to fit the window
+//                sceneNode.scaleMode = .aspectFill
+//                
+//                // Present the scene
+//                if let view = self.view as! SKView? {
+//                    view.presentScene(sceneNode)
+//                    
+//                    view.ignoresSiblingOrder = true
+//                    
+//                    view.showsFPS = true
+//                    view.showsNodeCount = true
+//                }
+//            }
+//        }
+//    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if let skview = self.view as? SKView {
+            if skview.scene == nil {
+                let aspectRatio = (skview.bounds.size.width)/(skview.bounds.size.height)
+                let scene = GameScene(size: CGSize(width: 320.0, height: 320.0 * aspectRatio))
+                skview.showsFPS = false
+                skview.ignoresSiblingOrder = true
+                skview.showsPhysics = false
+                skview.showsNodeCount = false
+                scene.scaleMode = .aspectFit
+                skview.presentScene(scene)
             }
         }
     }
-
     override var shouldAutorotate: Bool {
         return true
     }
